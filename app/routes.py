37c7555faafb4ce3fa1622aaea18828692c52af3
love-adelar <love.adelar@gmail.com>
2018-06-03 01:42:23 +0900

    Modified .gitignore

---
+++ app/routes.py
@@ -0,0 +1,21 @@
+from flask import render_template
+from app import app
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username':'Adela'}
+    posts = [
+        {
+            'author' : {'username': 'Jeong'},
+            'body' : 'Beautiful day in Seoul!'
+        },
+        {
+            'author' : {'username' : 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
---
+++ venv/lib/python3.6/enum.py
@@ -0,0 +1,1 @@
+/Users/Adelar/.pyenv/versions/3.6.0/lib/python3.6/enum.py
