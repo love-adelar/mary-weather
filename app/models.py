fcbe462b98e44d4ab68f0389e01dfc65fd78e810
love-adelar <love.adelar@gmail.com>
2018-11-12 23:34:54 +0900

    Created Database table

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from app import db
 
-class 
+
+class Date(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, primary_key=True)
+    min_temperature = db.Column(db.Float)
+    max_temperature = db.Column(db.Float)
+    weather_type = db.Column(db.String(128))
+    rainfall_probability = db.Column(db.Float)
+
+    def __repr__(self):
+        return '<Today is {}>'.format(self.date)
