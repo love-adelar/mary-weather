c01a4a50a9fa4a5963cde33319028c7d1934b0f6
love-adelar <love.adelar@gmail.com>
2018-11-20 21:35:03 +0900

    DB 수정

--- app/models.py
+++ app/models.py
@@ -1,13 +1,24 @@
+from datetime import datetime
 from app import db
+import pytz
 
 
-class Date(db.Model):
+class Weather(db.Model):
+    __tablename__ = 'weathers'
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.Date, primary_key=True)
-    min_temperature = db.Column(db.Float)
-    max_temperature = db.Column(db.Float)
+    today = db.Column(db.DateTime, primary_key=True, default=datetime.now(pytz.timezone('Asia/Seoul')))
+    temperature = db.Column(db.Float)
     weather_type = db.Column(db.String(128))
     rainfall_probability = db.Column(db.Float)
 
+    def __init__(self, date, today, temperature, weather_type, rainfall_probability):
+        self.date = date
+        self.today = today
+        self.temperature = temperature
+        self.weather_type = weather_type
+        self.rainfall_probability = rainfall_probability
+
     def __repr__(self):
-        return '<Today is {}>'.format(self.date)
+        return '<오늘 {}의 최저기온은 {}, and 최대기온은 {}입니다. 강수확률은 {}로 {}이겠습니다. >'\
+            .format(self.today, self.low, self.high, self.rainfall_probability, self.weather_type)
+
