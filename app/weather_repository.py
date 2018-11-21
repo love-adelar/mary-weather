799d8c938866efacd2e9731266c25db538d1d43e
love-adelar <love.adelar@gmail.com>
2018-11-21 15:43:16 +0900

    Error API, html 템플릿 추가 및 수정

---
+++ app/weather_repository.py
@@ -0,0 +1,68 @@
+import app
+from app import models
+import urllib.requests
+import ssl
+from bs4 import BeautifulSoup
+from datetime import datetime
+import pytz
+from sqlalchemy import asc
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+class WeatherRepository:
+    def __init__(self, weather_type):
+        self.weather_type = weather_type
+        self.weather = {
+            'sunny': '맑음',
+            'cloudy_less': '구름조금',
+            'cloudy_much': '구름많음',
+            'overcast': '흐림',
+            'foggy': '안개',
+            'rain': '비',
+            'drizzle': '이슬비',
+            'shower': '소나기',
+            'lightens_and_thunders': '천둥번개',
+            'sleet': '진눈깨비',
+            'snow': '눈'
+        }[weather_type]
+
+    @staticmethod
+    def create_db():
+        app.models.create_db()
+
+    def print_weather(self):
+        session = models.Session()
+        for row in session.query(models.Weather) \
+                .filter(models.Weather.weather_type==self.weather_type) \
+                .order_by(asc(models.Weather.date)) \
+                .all():
+            print("{} 오늘 날씨 \n {}: {}°C, {}, 강수확률 {}%")\
+                .format(row.today, row.meridiem, row.temperature, row.weather_type, row.rain_probability)
+
+    def get_weather(self, end_date=None, duration=365):
+        if end_date is None:
+            end_date = datetime.datetime.now()
+
+        start_date = end_date - datetime.timedelta(duration)
+        session = models.Session()
+
+        url = "https://weather.naver.com/rgn/townWetr.nhn?naverRgnCd=09230109"
+        page = urllib.request.urlopen(url)
+        weather_type = BeautifulSoup(page, "html.parser")
+
+        today = datetime.now(pytz.timezone('Asia/Seoul')).strftime("%y년 %m월 %d일")
+        subject_text = "{} 오늘 날씨\n".format(today)
+
+        for cell in weather_type.find_all("div", "cell", limit=2):
+            meridiem = cell.find("b").string
+            temperature = cell.find("span", "temp").string
+            weather_type = cell.img['alt']
+            rain_probability = cell.find("strong").string
+            subject_text += "{}: {}°C, {}, 강수확률 {}%\n".format(meridiem, temperature, weather_type, rain_probability)
+
+            session.merge(models.Weather())
+            # print(subject_text)
+        session.commit()
+
+
