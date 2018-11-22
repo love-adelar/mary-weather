11e975313f862c57971e859965b63b86f88016c3
love-adelar <love.adelar@gmail.com>
2018-11-22 12:27:09 +0900

    UI 변경

--- app/models.py
+++ app/models.py
@@ -6,19 +6,20 @@ import pytz
 class Weather(db.Model):
     __tablename__ = 'weathers'
     id = db.Column(db.Integer, primary_key=True)
-    today = db.Column(db.DateTime, primary_key=True, default=datetime.now(pytz.timezone('Asia/Seoul')))
+    date = db.Column(db.DateTime, default=datetime.now(pytz.timezone('Asia/Seoul')))
+    meridiem = db.Column(db.String(64))
     temperature = db.Column(db.Float)
     weather_type = db.Column(db.String(128))
     rainfall_probability = db.Column(db.Float)
+    # image_filename = "photo-1511765224389-37f0e77cf0eb"
 
-    def __init__(self, date, today, temperature, weather_type, rainfall_probability):
+    def __init__(self, date, meridiem, temperature, weather_type, rainfall_probability):
         self.date = date
-        self.today = today
+        self.meridiem = meridiem
         self.temperature = temperature
         self.weather_type = weather_type
         self.rainfall_probability = rainfall_probability
 
     def __repr__(self):
-        return '<오늘 {}의 최저기온은 {}, and 최대기온은 {}입니다. 강수확률은 {}로 {}이겠습니다. >'\
-            .format(self.today, self.low, self.high, self.rainfall_probability, self.weather_type)
-
+        return "<{} 오늘 {} 날씨: {}°C, {}, 강수확률 {}% >" \
+            .format(self.date, self.meridiem, self.temperature, self.weather_type, self.rainfall_probability)
